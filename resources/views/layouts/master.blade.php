94997797ad33d17d590b40f6444bba399855b298
faruk <fukephp@gmail.com>
2022-01-14 14:26:22 +0100

    Setup master layout Add bootstrap libary Login/Register simple auth

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.87.0">
+    <title>Check Hotels</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="{!! url('assets/css/main.css') !!}" rel="stylesheet">
+</head>
+<body>
+    
+    @include('layouts.partials.navbar')
+
+    <main class="container">
+        <section class="main">
+        	
+        	@include('layouts.partials.messages')
+
+        	@yield('content')
+        </section>
+    </main>
+
+    @include('layouts.partials.footer')
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+	<p class="mt-5 mb-3 text-muted text-center">CheckHotels &copy; {{date('Y')}}</p>
+</footer>
